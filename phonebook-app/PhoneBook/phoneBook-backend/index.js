0a9d5d3f77d58dbcd7eab5dbc6cd6c7861588678
JP Robbins <jp.robbins22@gmail.com>
2023-10-04 15:32:45 -0400

    uibuild

--- phonebook-app/PhoneBook/phoneBook-backend/index.js
+++ phonebook-app/PhoneBook/phoneBook-backend/index.js
@@ -1,50 +1,15 @@
 
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
 const cors = require('cors')
-const mongoose = require('mongoose')
+const Person = require('./models/person')
 
 app.use(morgan(':method :status :total-time[2]'))
 app.use(express.json())
 app.use(cors())
 app.use(express.static('dist'))
-
-let persons = [
-    {
-        "id": 1,
-        "name": "Arto Hellas",
-        "number": "040-123456"
-      },
-      {
-        "id": 2,
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523"
-      },
-      {
-        "id": 3,
-        "name": "Dan Abramov",
-        "number": "12-43-234345"
-      },
-      {
-        "id": 4,
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122"
-      }
-    ]
-
-const url =
-`mongodb+srv://jprobbins:${password}@cluster0.kbixiln.mongodb.net/phonebookApp?retryWrites=true&w=majority`
-
-mongoose.set('strictQuery', false)
-mongoose.connect(url)
-
-const personSchema = new mongoose.Schema({
-    name: String,
-    number: Number,
-})
-
-const Person = mongoose.model('Person', personSchema)
 
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
@@ -60,26 +25,45 @@ app.get('/api/persons', (request, response) => {
  })
 })
 
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint'})
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name == 'CastError') {
+        return response.status(400).send({ error: 'malformatted id'})
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
+    }
+   next(error)
+}
+
+// this has to be the last  loaded middleware.
+app.use(errorHandler)
+
+
 app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(p =>  p.id === id)
+    Person.findById(request.params.id)
+    .then(person => {
         if (person) {
             response.json(person)
         } else {
-            response.status(204).end()
+            response.status(404).end()
         }
-    }
-)
+    })
+    .catch(error => next(error))
+    })
 
 app.delete('/api/persons/:id', (request, response) => {
-   const id = Number(request.params.id)
-   const person = persons.find(p => p.id === id)
-
-   if (person) {
-     response.send(`${person.name} deleted from server.`)
-   } else {
+   Person.findByIdAndRemove(request.params.id)
+   .then(result => {
     response.status(204).end()
-   }
+   })
+   .catch(error => next(error))
 })
 
 const generateId = () => {
@@ -89,26 +73,21 @@ const generateId = () => {
     return maxId + 1
 }
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
 
-    if (!body.name) {
-        return response.status(400).json({
-            error: 'name missing'
-        })
-    }
-
-    const person = {
+    const person = new Person({
         name: body.name,
-        number: body.number,
-        id: generateId(),
-    }
+        number: body.number
+    })
 
     const checkDuplicates = persons.find(p => p.name === person.name)
 
     if (!checkDuplicates) {
-    persons = persons.concat(person)
-    response.json(person)
+    person.save()
+    .then(savedPerson => {
+        response.json(savedPerson)
+    })
     } else {
         return response.status(404).json({
             error: 'Name must be unique'
@@ -116,7 +95,21 @@ app.post('/api/persons', (request, response) => {
     }
 })
 
-const PORT = process.env.PORT || 3001
+app.put('/api/person', (request, response, next) => {
+    const { name, number } = request.body
+   
+    Person.findByIdAndUpdate(
+        request.params.id,
+        { name, number },
+        { new: true, runValidators: true, context: 'query' }
+    )
+    .then(updatedPerson => {
+        response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
+const PORT = process.env.PORT
 app.listen(PORT, ()=> {
 console.log(`server running on port: ${PORT}`)
 })
